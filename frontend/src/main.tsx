4e5fbf365d9638a73115d82a9ff72dfd7d01ecff
Sahil <sahilkhatri312@gmail.com>
2026-05-14 00:48:28 +0530

    fix: added basename to BrowserRouter for github pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -20,7 +20,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
           <App />
         </BrowserRouter>
       </QueryClientProvider>
